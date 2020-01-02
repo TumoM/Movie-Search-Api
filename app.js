d39f23a362dcdfac424a5b3921fcd880d79b08aa
tumom <tumomasire@gmail.com>
2020-01-03 01:18:14 +0200

    Search Page + Redirect working

--- app.js
+++ app.js
@@ -5,25 +5,30 @@ const locus = require("locus");
 var app = express();
 app.set("view engine", "ejs");
 
-app.get('/results/:search*?',function(req, res){
-    var search = "california"
-    if(req.params.search){
-        search = req.params.search;
-          }
-    request(`http://www.omdbapi.com/?s=${search}&apikey=thewdb`, function(err, response, body){
+app.get('/results', function (req, res) {
+    var search = "california";
+    if (req.query.search) {
+        search = req.query.search;
+    }
+    request(`http://www.omdbapi.com/?s=${search}&apikey=thewdb`, function (err, response, body) {
         if (!err && response.statusCode == 200) {
             var parsedBody = JSON.parse(body);
             // res.send(parsedBody.Search[0]);
-            res.render("results", {results: parsedBody})
+            res.render("results", {
+                results: parsedBody
+            })
         } else {
             console.log("ERRRRROR!!!");
             console.log(err);
         }
-
     })
 })
 
-app.listen(3000,function(){
+app.get("/", function (req, res) {
+    res.render("search")
+})
+
+app.listen(3000, function () {
     console.log("Listening on port 3000");
 
 })
